a966525b423a1562f93b96225bfbfa977728559e
ZZ <z2331078@163.com>
2019-01-04 10:53:43 +0800

    新增nfc编码管理功能

---
+++ pig-modules/pig-upms-service/src/main/java/com/github/pig/admin/mapper/NfcCodeMapper.java
@@ -0,0 +1,16 @@
+package com.github.pig.admin.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.github.pig.admin.model.entity.NfcCode;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zzz
+ * @since 2018-12-25
+ */
+public interface NfcCodeMapper extends BaseMapper<NfcCode> {
+
+}
